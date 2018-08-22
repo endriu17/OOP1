9dc86ab1ac957e258571fea5004af1ae723ec781
endriu17 <aolaszewski@gmail.com>
2018-08-22 19:42:44 +0200

    fix js files

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 (function(){
 
-  function Phone(brand, price, color) {
+  function Phone(brand, price, color, os, memory) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
+  this.os = os;
+  this.memory = memory;
 }
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "." + " Operating system: " + this.os + " and memory size is " + this.memory + ".");
 }
 
-  var iPhone6S = new Phone("Apple", 2250, "silver");
-  var samsungGalS6 = new Phone("Samsung Galaxy S6", 1500, "black");
-  var onePlusOne  = new Phone("OnePlus One", 1800, "gold");
+  var iPhone6S = new Phone("Apple", 2250, "silver", "iOS 11", "64GB");
+  var samsungGalS6 = new Phone("Samsung Galaxy S6", 1500, "black", "Android 7","32GB");
+  var onePlusOne  = new Phone("OnePlus One", 1800, "gold", "Android 8","128GB");
   iPhone6S.printInfo();
   samsungGalS6.printInfo();
   onePlusOne.printInfo();
